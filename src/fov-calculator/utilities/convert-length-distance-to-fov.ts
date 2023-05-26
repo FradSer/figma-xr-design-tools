56f8c9f53f4e9ee3f4e6285f4628a986a29331f3
Frad LEE <fradser@gmail.com>
2023-05-26 11:31:47 +0800

    refactor: update utilities

--- src/fov-calculator/utilities/convert-length-distance-to-fov.ts
+++ src/fov-calculator/utilities/convert-length-distance-to-fov.ts
@@ -2,14 +2,15 @@ export function convertLengthDistanceToFOV(
   length: number,
   distance: null | number
 ) {
-  let _distance = distance === null ? 0 : distance
+  let actualDistance = distance === null ? 0 : distance
 
-  if (length == 0 || _distance == 0) {
+  if (length === 0 || actualDistance === 0) {
     return null
   }
 
-  return (
-    (Math.atan(length / 2 / (_distance / 100) / 1000) / (Math.PI / 180)) *
-    2
-  ).toFixed(2)
+  const fovRadians = Math.atan(length / 2 / (actualDistance / 100) / 1000)
+
+  const fovDegrees = ((fovRadians / (Math.PI / 180)) * 2).toFixed(2)
+
+  return fovDegrees
 }
--- src/fov-guide-generator/utilities/convert-fov-distance-to-length.ts
+++ src/fov-guide-generator/utilities/convert-fov-distance-to-length.ts
@@ -2,14 +2,24 @@ export function convertFOVDistanceToLength(
   fov: null | number,
   distance: null | number
 ) {
-  let _distance = distance === null ? 0 : distance
-  let _fov = fov === null ? 0 : fov
+  const defaultDistance = 0
+  const defaultFOV = 0
 
-  const vault = Math.ceil(
-    2 * _distance * Math.tan((_fov * (Math.PI / 180)) / 2) * 10
-  )
-  if (vault % 2 != 0) {
-    return vault + 1
+  const actualDistance = distance ?? defaultDistance
+  const actualFOV = fov ?? defaultFOV
+
+  if (actualFOV < 0 || actualDistance < 0) {
+    return 0
   }
-  return vault
+
+  const vaultLength = Math.ceil(
+    2 * actualDistance * Math.tan((actualFOV * Math.PI) / 360) * 10
+  )
+
+  const isVaultLengthOdd = vaultLength % 2 !== 0
+  if (isVaultLengthOdd) {
+    return vaultLength + 1
+  }
+
+  return vaultLength
 }
